02c872c2d07ad63871c9b3b80c9649acc03a2ada
Richard Valentino Parulian Silalahi <silalahirichardvp@gmail.com>
2026-04-05 23:27:12 +0800

    Fix formatting issues in data.js descriptions

--- v2/assets/js/data.js
+++ v2/assets/js/data.js
@@ -4,37 +4,49 @@ window.S2S_DATA = {
       id: 1,
       kata: "A",
       kategori: "alfabet",
-      deskripsi: "Bentuk tangan mengepal dengan ibu jari di samping."
+      deskripsi: "Bentuk tangan mengepal dengan ibu jari di samping.",
+      image: "../assets/img/kamus/alfabet-a.svg",
+      alt: "Ilustrasi isyarat huruf A"
     },
     {
       id: 2,
       kata: "B",
       kategori: "alfabet",
-      deskripsi: "Telapak tangan terbuka, jari rapat dan tegak."
+      deskripsi: "Telapak tangan terbuka, jari rapat dan tegak.",
+      image: "../assets/img/kamus/alfabet-b.svg",
+      alt: "Ilustrasi isyarat huruf B"
     },
     {
       id: 3,
       kata: "1",
       kategori: "angka",
-      deskripsi: "Tunjukkan jari telunjuk ke atas."
+      deskripsi: "Tunjukkan jari telunjuk ke atas.",
+      image: "../assets/img/kamus/angka-1.svg",
+      alt: "Ilustrasi isyarat angka 1"
     },
     {
       id: 4,
       kata: "2",
       kategori: "angka",
-      deskripsi: "Tunjukkan dua jari: telunjuk dan tengah."
+      deskripsi: "Tunjukkan dua jari: telunjuk dan tengah.",
+      image: "../assets/img/kamus/angka-2.svg",
+      alt: "Ilustrasi isyarat angka 2"
     },
     {
       id: 5,
       kata: "Halo",
       kategori: "salam",
-      deskripsi: "Gerakan tangan seperti menyapa dari dekat dahi."
+      deskripsi: "Gerakan tangan seperti menyapa dari dekat dahi.",
+      image: "../assets/img/kamus/salam-halo.svg",
+      alt: "Ilustrasi isyarat halo"
     },
     {
       id: 6,
       kata: "Terima Kasih",
       kategori: "salam",
-      deskripsi: "Ujung jari menyentuh dagu lalu bergerak ke depan."
+      deskripsi: "Ujung jari menyentuh dagu lalu bergerak ke depan.",
+      image: "../assets/img/kamus/salam-terima-kasih.svg",
+      alt: "Ilustrasi isyarat terima kasih"
     }
   ],
   quiz: [
